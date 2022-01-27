bbe72b08c0817d0c81eafec3bc0722918d9889b0
Jules Hummelink <julescitronic.nexus@gmail.com>
2022-01-27 13:57:22 +0100

    Last ip fix

--- server.py
+++ server.py
@@ -296,7 +296,7 @@ app = flask.Flask(__name__)
 # Landing page
 @app.route("/")
 def main():
-    return render_template('main.html', ip=request.access_route)
+    return render_template('main.html', ip=request.access_route[-1])
 
 
 # Search API
